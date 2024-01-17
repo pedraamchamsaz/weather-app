deae237f19b6bb10bf6d7e54f14e137a892d8835
Ilona <lona.krechko@gmail.com>
2024-01-17 15:52:46 +0000

    Pushed data from API into current component

--- app/page.js
+++ app/page.js
@@ -19,14 +19,12 @@ export default function Home() {
       const response = await axios.get(`/api/weather?city=${city}`);
       console.log(response.data, "DATA");
       setWeather(response.data.body);
-      // console.log(weather);
     } catch (error) {
       console.log(error, "THIS IS AN ERROR");
     }
   };
 
   useEffect(() => {
-    //GET();
     handleSearch();
   }, []);
 
@@ -41,21 +39,16 @@ export default function Home() {
             placeholder="Enter location"
             onChange={onChangeCity}
           />
-          {/* <svg
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             x="0px"
             y="0px"
             className="w-6 h-6 object-contain"
             viewBox="0 0 50 50"
             onClick={handleSearch}
-          > */}
-
-          {/* <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
-          </svg> */}
-
-          <button type="submit" onClick={handleSearch}>
-            Search
-          </button>
+          >
+            <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
+          </svg>
         </div>
         <div className="flex w-full justify-center">
           <p>C</p>
